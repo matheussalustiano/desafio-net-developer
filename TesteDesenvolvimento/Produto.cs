596b0b9ba51adc2078d0183b12849db1121aaffa
Matheus Salustiano <matheus.salustiano@onlineapp.com.br>
2025-07-03 17:40:43 -0300

    Add project files.

---
+++ TesteDesenvolvimento/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace TesteDesenvolvimento
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
